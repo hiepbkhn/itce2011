e014133ffc3cd0112329cd4892f89117db189eaa
hiepnh <hiepnh.vtt@gmail.com>
2015-06-07 18:16:25 +0200

    first commit

---
+++ local-dp-java/src/pdp/PersonalizedDP.java
@@ -0,0 +1,24 @@
+/**
+ * Jun 7
+ * papers:
+ *  - Conservative or Liberal - Personalized Differential Privacy (ICDE'15)
+ * 	
+ */
+
+package pdp;
+
+public class PersonalizedDP {
+
+	////
+	public static void samplingMechanism(){
+		
+		
+	}
+	
+	////
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
