728210508d590f48d80ac9d937b44f2bd84ff9d2
hiepnh <hiepnh.vtt@gmail.com>
2016-05-26 00:59:59 +0200

    greedyVertexCoverNaive()

--- local-dp-java/src/dsn/LinkExchange.java
+++ local-dp-java/src/dsn/LinkExchange.java
@@ -20,6 +20,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -663,7 +664,7 @@ public class LinkExchange {
 	}
 	
 	////
-	public static int binarySearch(List<Int2> edges, int u){
+	public static int binarySearch(List<Int2> edges, Int2 target){
 		int ret = -1;
 		
 		int lo = 0;
@@ -673,6 +674,44 @@ public class LinkExchange {
 		boolean found = false;
 		while (true){
 			Int2 e = edges.get(mid);
+			
+			if (e.compareTo(target) == -1){
+				lo = mid + 1;
+			}else if (e.compareTo(target) == 1){
+				hi = mid - 1;
+			}else{
+				found = true;
+				break;
+			}
+			
+			mid = (lo + hi)/2;
+			
+			if (lo > hi)
+				break;
+		}
+		
+		if (!found)
+			ret = -1;
+		else{
+			ret = mid;
+		}
+		
+		//
+		return ret;
+	}
+	
+	////
+	public static int binarySearchVertex(List<Int2> edges, int u){
+		int ret = -1;
+		
+		int lo = 0;
+		int hi = edges.size()-1;
+		int mid = (lo + hi)/2;
+		
+		boolean found = false;
+		while (true){
+			Int2 e = edges.get(mid);
+			
 			if (e.val0 < u){
 				lo = mid + 1;
 			}else if (e.val0 > u){
@@ -692,13 +731,8 @@ public class LinkExchange {
 			ret = -1;
 		else{
 			ret = mid;
-			while (ret > 0){		// find the leftmost
-				if (edges.get(ret).val0 == u)
-					ret = ret - 1;
-				else
-					break;
-			}
-				
+			while (ret > 0 && edges.get(ret).val0 == u)
+				ret = ret - 1;
 		}
 		
 		//
@@ -706,7 +740,7 @@ public class LinkExchange {
 	}
 	
 	////
-	public static void approxVertexCover(EdgeIntGraph G){
+	public static void approxVertexCoverNaive(EdgeIntGraph G){
 		int n = G.V();
 		
 		List<Int2> edges = new ArrayList<Int2>();
@@ -715,10 +749,6 @@ public class LinkExchange {
 			int v = e.other(u);
 			edges.add(new Int2(u,v));
 		}
-		
-		// sort edges
-		normalizeEdges(edges);
-		Collections.sort(edges);
 		
 		//
 		List<Integer> cover = new ArrayList<Integer>();
@@ -732,25 +762,21 @@ public class LinkExchange {
 			int v = e.val1;
 			cover.add(u);
 			cover.add(v);
-//			System.out.println(u + "-" + v);
 			
 //			if (cover.size() % 1000 == 0)
 //				System.out.println(cover.size());
 			
 			// remove all edges containing u,v
-			id = binarySearch(edges, u);
-			System.out.println(id + " " + edges.size());
-			while (edges.get(id).val0 == u)
-				edges.remove(id);
-			id += 1;
-			if (id == edges.size())
-				break;
-			
-			id = binarySearch(edges, v);
-			if (id == -1)
-				continue;
-			while (edges.get(id).val0 == v)
-				edges.remove(id);
+			id = 0;
+			while (id < edges.size()){
+				e = edges.get(id);
+				int u1 = e.val0;
+				int v1 = e.val1;
+				if (u1 == u || u1 == v || v1 == u || v1 == v){
+					edges.remove(id);
+				}else
+					id += 1;
+			}
 			
 		}
 		
@@ -765,6 +791,290 @@ public class LinkExchange {
 	}
 	
 	
+	//// faster
+	public static void approxVertexCover(EdgeIntGraph G){
+//		int n = G.V();
+//		
+//		List<Int2> edges = new ArrayList<Int2>();
+//		for (EdgeInt e : G.edges()){
+//			int u = e.either();
+//			int v = e.other(u);
+//			edges.add(new Int2(u,v));
+//		}
+//		
+//		// sort edges
+//		normalizeEdges(edges);
+//		Collections.sort(edges);
+//		
+//		//
+//		List<Integer> cover = new ArrayList<Integer>();
+//		Random random = new Random();
+//		while (edges.size() > 0){
+//			// pick a random edge
+//			int id = random.nextInt(edges.size());
+//			Int2 e = edges.get(id);
+//			
+//			int u = e.val0;		// u < v
+//			int v = e.val1;
+//			cover.add(u);
+//			cover.add(v);
+//			
+//				if (cover.size() % 1000 == 0)
+//					System.out.println(cover.size());
+//			
+//			// remove all edges containing u,v
+//			boolean stop = false;
+//			for (int v1 : G.adj(u).keySet()){
+//				e = new Int2(u, v1);
+//				normalizeEdge(e);
+//				
+//				id = binarySearch(edges, e);
+//				if (id != -1)
+//					edges.remove(id);
+//				
+//				if (edges.size() == 0){
+//					stop = true;
+//					break;
+//				}
+//			}
+//			if (stop)
+//				break;
+//			
+//			for (int u1 : G.adj(v).keySet()){
+//				e = new Int2(u1, v);
+//				normalizeEdge(e);
+//				
+//				id = binarySearch(edges, e);
+//				if (id != -1)
+//					edges.remove(id);
+//				
+//				if (edges.size() == 0){
+//					stop = true;
+//					break;
+//				}
+//			}
+//			if (stop)
+//				break;
+//			
+//			
+//		}
+		
+		//////////////// FASTER ?? no faster on amazon
+		int n = G.V();
+		
+		List<Int2> edges = new ArrayList<Int2>();
+		for (EdgeInt e : G.edges()){
+			int u = e.either();
+			int v = e.other(u);
+			edges.add(new Int2(u,v));
+		}
+		
+		List<HashSet<Integer>> adj = new ArrayList<HashSet<Integer>>();
+		for (int u = 0; u < n; u++){
+			adj.add(new HashSet<Integer>());
+			for (int v : G.adj(u).keySet())
+				adj.get(u).add(v);
+		}
+		
+		
+		// sort edges
+		normalizeEdges(edges);
+		Collections.sort(edges);
+		
+		System.out.println("here");
+		
+		//
+		List<Integer> cover = new ArrayList<Integer>();
+		Random random = new Random();
+		while (edges.size() > 0){
+			// pick a random edge
+			int id = random.nextInt(edges.size());
+			Int2 e = edges.get(id);
+			
+			int u = e.val0;		// u < v
+			int v = e.val1;
+			cover.add(u);
+			cover.add(v);
+			
+			// debug
+			if (cover.size() % 1000 == 0)
+				System.out.println(cover.size());
+			
+			// remove all edges containing u,v
+			boolean stop = false;
+			List<Integer> temp = new ArrayList<Integer>();
+			
+			// node u
+			id = binarySearchVertex(edges, u);
+			if (id != -1 && edges.size() > 0){
+				while(id < edges.size() && edges.get(id).val0 == u){
+					int v1 = edges.get(id).val1;
+					edges.remove(id);
+					adj.get(u).remove(v1);
+					adj.get(v1).remove(u);
+				}
+			}
+			
+			for (int v1 : adj.get(u)){
+				e = new Int2(u, v1);
+				normalizeEdge(e);
+				
+				id = binarySearch(edges, e);
+				if (id != -1){
+					edges.remove(id);
+					temp.add(v1);
+					adj.get(v1).remove(u);
+					
+					if (edges.size() == 0){
+						stop = true;
+						break;
+					}
+				}
+				
+			}
+			adj.get(u).removeAll(temp);
+			if (stop || edges.size() == 0)
+				break;
+			
+			// node v
+			id = binarySearchVertex(edges, v);
+			if (id != -1 && edges.size() > 0){
+				while(id < edges.size() && edges.get(id).val0 == v){
+					int u1 = edges.get(id).val1;
+					edges.remove(id);
+					adj.get(v).remove(u1);
+					adj.get(u1).remove(v);
+				}
+			}
+			
+			temp = new ArrayList<Integer>();
+			for (int u1 : G.adj(v).keySet()){
+				e = new Int2(u1, v);
+				normalizeEdge(e);
+				
+				id = binarySearch(edges, e);
+				if (id != -1){
+					edges.remove(id);
+					temp.add(u1);
+					adj.get(u1).remove(v);
+					
+					if (edges.size() == 0){
+						stop = true;
+						break;
+					}
+				}
+			}
+			adj.get(v).removeAll(temp);
+			if (stop)
+				break;
+			
+			
+		}
+		
+		//
+		System.out.println("cover.size = " + cover.size());
+		if (cover.size() < 100){
+			for (int u : cover)
+				System.out.print(u + " ");
+			System.out.println();
+		}
+	}
+	
+	//// select top-degree nodes (deterministic)
+	public static void greedyVertexCoverNaive(EdgeIntGraph G){
+		int n = G.V();
+		List<Integer> cover = new ArrayList<Integer>();
+		int[] deg = new int[n];
+		boolean[] marked = new boolean[n];
+		
+		for (int u = 0; u < n; u++){
+			deg[u] = G.degree(u);
+			marked[u] = false;
+		}
+		
+		
+		int count = 0;
+		while (true){
+			// find top-degree node
+			int top = -1;
+			for (int u = 0; u < n; u++)
+				if (marked[u] == false){
+					top = u;
+					break;
+				}
+			for (int u = 0; u < n; u++)
+				if (marked[u] == false && deg[top] < deg[u])
+					top = u;
+			//
+			cover.add(top);
+			marked[top] = true;
+			
+			// debug
+			if (cover.size() % 1000 == 0)
+				System.out.println(cover.size());
+			
+			// update degrees
+			for (int v : G.adj(top).keySet())
+				if (marked[v] == false){
+					count += 1;
+				}
+			
+			if (count == G.E())
+				break;
+		}
+		
+		/////////////// SLOWER due to SET OPERATIONS
+//		int n = G.V();
+//		List<Integer> cover = new ArrayList<Integer>();
+//		int[] deg = new int[n];
+//		boolean[] marked = new boolean[n];
+//		HashSet<Integer> nodes = new HashSet<Integer>();
+//		
+//		for (int u = 0; u < n; u++){
+//			deg[u] = G.degree(u);
+//			marked[u] = false;
+//			nodes.add(u);
+//		}
+//		
+//		int count = 0;
+//		while (true){
+//			// find top-degree node
+//			int top = -1;
+//			for (int u : nodes)
+//				if (top == -1)
+//					top = u;
+//				else if (deg[top] < deg[u])
+//					top = u;
+//			//
+//			cover.add(top);
+//			marked[top] = true;
+//			nodes.remove(top);
+//			
+//			// debug
+//			if (cover.size() % 1000 == 0)
+//				System.out.println(cover.size());
+//			
+//			// update degrees
+//			for (int v : G.adj(top).keySet())
+//				if (marked[v] == false){
+//					count += 1;
+//				}
+//			
+//			if (count == G.E())
+//				break;
+//		}
+		
+		
+		//
+		System.out.println("cover.size = " + cover.size());
+		if (cover.size() < 100){
+			for (int u : cover)
+				System.out.print(u + " ");
+			System.out.println();
+		}
+		
+	}
+	
 	////////////////////////////////////////////////
 	public static void main(String[] args) throws Exception{
 		String prefix = "";
@@ -773,21 +1083,21 @@ public class LinkExchange {
 //		String dataname = "pl_1000_5_01";		// diameter = 5
 //		String dataname = "pl_10000_5_01";		// diameter = 6,  Dup: round=3 (OutOfMem, 7GB ok), 98s (Acer)
 												//				NoDup: round=3 (4.5GB), 376s (Acer)
-												// cover.size = 8278
+												// cover.size = 8278,	cover.size = 7171 (greedy)
 //		String dataname = "ba_1000_5";			// diameter = 5
 //		String dataname = "ba_10000_5";			// diameter = 6, NoDup: round=3 (5.1GB), 430s (Acer), 350s (PC), totalLink = 255633393
-												// cover.size = 8384
+												// cover.size = 8384,	cover.size = 7167 (greedy)	
 		
 //		String dataname = "er_1000_001";		// diameter = 5
 //		String dataname = "er_10000_0001";		// diameter = 7, NoDup: round=3 (2.5GB), 23s (PC)
-												// cover.size = 9076
+												// cover.size = 9076,	cover.size = 9952 (greedy)	
 //		String dataname = "sm_1000_005_11";		// diameter = 9
 //		String dataname = "sm_10000_005_11";	// diameter = 12, NoDup: round=3 (1.2GB), 5s (PC), round=4 (1.7GB), 12s (PC)
 												// 						round=5 (3.0GB), 29s (PC), round=6 (3.3GB), 74s (PC)
-												// cover.size = 9448
+												// cover.size = 9448,	cover.size = 9987 (greedy)	
 		//
 //		String dataname = "example";			// 		diameter = 5, cover.size = 10
-		String dataname = "karate";				// (34, 78)	diameter = 5, cover.size = 20
+//		String dataname = "karate";				// (34, 78)	diameter = 5, cover.size = 20
 //		String dataname = "polbooks";			// (105, 441)		
 //		String dataname = "polblogs";			// (1224,16715) 	
 //		String dataname = "as20graph";			// (6474,12572)		
@@ -795,9 +1105,9 @@ public class LinkExchange {
 //		String dataname = "ca-HepPh";			// (12006,118489) 	
 //		String dataname = "ca-AstroPh";			// (18771,198050) 			
 		// LARGE
-//		String dataname = "com_amazon_ungraph";		// (334863,925872)	
-//		String dataname = "com_dblp_ungraph";		// (317080,1049866)	
-//		String dataname = "com_youtube_ungraph";	// (1134890,2987624) 
+//		String dataname = "com_amazon_ungraph";		// (334863,925872)	, cover.size = 240510 (43s, Acer), cover.size = 309152 (greedy)
+//		String dataname = "com_dblp_ungraph";		// (317080,1049866)	, cover.size = 225870 (63s, Acer)
+		String dataname = "com_youtube_ungraph";	// (1134890,2987624) 
 		
 		
 		//
@@ -877,7 +1187,15 @@ public class LinkExchange {
 		
 		/////////
 		// TEST approxVertexCover()
-		approxVertexCover(G);
+//		approxVertexCoverNaive(G);
+		
+//		start = System.currentTimeMillis();
+//		approxVertexCover(G);
+//		System.out.println("approxVertexCover - DONE, elapsed " + (System.currentTimeMillis() - start));
+		
+		start = System.currentTimeMillis();
+		greedyVertexCoverNaive(G);
+		System.out.println("greedyVertexCoverNaive - DONE, elapsed " + (System.currentTimeMillis() - start));
 		
 	}
 
